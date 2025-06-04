02770101535a1ac51517cbd6c1763617014205bc
Kenny Hung (he) <kenny.cfg@outlook.com>
2025-06-04 19:18:24 +0100

    Working regex example

--- src/main/java/com/kenny/wenny/performance/RegexDemo.java
+++ src/main/java/com/kenny/wenny/performance/RegexDemo.java
@@ -1,7 +1,15 @@
 package com.kenny.wenny.performance;
 
+import lombok.val;
+
+import java.util.regex.Pattern;
+
 public class RegexDemo {
-    public boolean contains10DigitNumber(String number) {
-        return true;
+    private static final String REGEX = "\\d{10}";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
+
+    public boolean contains10DigitNumber(String source) {
+        val matcher = PATTERN.matcher(source);
+        return matcher.find();
     }
 }
