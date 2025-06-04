ae3d839091b4199be4a81cabf161826f5633689b
Kenny Hung (he) <kenny.cfg@outlook.com>
2025-06-04 19:48:29 +0100

    Largest integer using streams

--- src/main/java/com/kenny/wenny/performance/LargestInteger.java
+++ src/main/java/com/kenny/wenny/performance/LargestInteger.java
@@ -1,11 +1,16 @@
 package com.kenny.wenny.performance;
 
-import lombok.val;
-
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Optional;
 
 public class LargestInteger {
     public Optional<Integer> findLargest(int[] numList) {
+        return Arrays.stream(numList)
+                .boxed()
+                .max(Comparator.naturalOrder());
+
+        /* Classic version
         Integer largestNumber = null;
         for (val number : numList) {
             if (largestNumber == null || largestNumber < number) {
@@ -13,5 +18,6 @@ public class LargestInteger {
             }
         }
         return Optional.ofNullable(largestNumber);
+         */
     }
 }
