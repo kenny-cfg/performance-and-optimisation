00cd4bf8ee893565b8cfd1c4656cf00744a8fec2
Kenny Hung (he) <kenny.cfg@outlook.com>
2025-06-04 19:28:21 +0100

    Multiples of 5 classic

---
+++ src/main/java/com/kenny/wenny/performance/StreamExample.java
@@ -0,0 +1,18 @@
+package com.kenny.wenny.performance;
+
+import lombok.val;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamExample {
+    public List<Integer> getMultiplesOf5(List<Integer> source) {
+        val multiplesOf5 = new ArrayList<Integer>();
+        for (val element : source) {
+            if (element % 5 == 0) {
+                multiplesOf5.add(element);
+            }
+        }
+        return multiplesOf5;
+    }
+}
